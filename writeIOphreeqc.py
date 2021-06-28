c98ae592483e2ebce2f8716f1e9d10d934371ee5
A-440Hz <htzeng@msn.com>
2021-06-28 12:58:06 -0700

    plot fix and RSS for MC Sim

--- writeIOphreeqc.py
+++ writeIOphreeqc.py
@@ -11,7 +11,7 @@ import re
 import numpy as np
 
 # write thermodynamic database with given parameters
-def writeInput(filename, pars, values):
+def writeInput(filename, pars, values, outfile=''):
     # filename: thermodynamic data base name
     # pars: parameter names
     # values: parameter values
@@ -19,23 +19,27 @@ def writeInput(filename, pars, values):
     c = f.readline()
     c = f.read()
 
-    # Replace the target string                                                                                                                                  
+    # Replace the target string
     for i in range(len(pars)):
         c = c.replace(pars[i], str(values[i]))
-
-    # Write the file out again                                                                                                                                   
-    with open(filename+'.txt', 'w') as f:
-        f.write(c)
-
+        
     # Close the file
     f.close()
+
+    if outfile == '':
+        outfile = filename
+    
+    # Write the file out again
+    with open(outfile+'.txt', 'w') as f:
+        f.write(c)
+
 
 
 # helper function to get col indices of "sim", "state", and "U" for use in readOutput
 # so it can be more robust with different phreeqc output formats
-def _getColIndices(file, key='U'):
+def _getColIndices(file, elems):
     # file: file object to read
-    # return: list of 3 indices for use in readOutput
+    # return: list of 2 indices and an analytes list for use in readOutput
     c = file.readline()
     headers = re.split(r"[\s,\"]+",c)
     
@@ -53,49 +57,51 @@ def _getColIndices(file, key='U'):
         print("error: state not found")
         print(headers)
         return ValueError
-    try:
-        U = headers.index(key)
-    except ValueError:
-        print("error: key \"{}\" not found".format(key))
-        print(headers)
-        return ValueError
+    analytes = []
+    for elem in elems:
+        try:
+            idx = headers.index(elem)
+            analytes.append(idx)            
+        except ValueError:
+            print("error: key \"{}\" not found".format(elem))
+            print(headers)
+            return ValueError
     
     # print(headers)
-    return [sim, state, U]
+    return [sim, state, analytes]
     
 # read PHREEQC output
-def readOutput(filename, analyte='U'):
+def readOutput(filename, analytes=['U']):
     # filename: output .sel file name
-    # analyte: name of analyte to find. defaults to U concentration
-    # return: 2-row np array of simulation# vs analyte concentration
+    # analytes: list of analytes names to find. defaults to U concentration
+    # return: np array of simulation# vs analyte concentration
     
-    f = open(filename, 'r')
-    idcs = _getColIndices(f, analyte)
-    
-    # exit if failed to read columns
-    if idcs == ValueError:
-        print("failed to parse columns of {}".format(filename))
-        f.close()
-        return
-    
-    # assign indices and finish reading the file if getColIndicies didn't error
-    i_sm, i_st, i_U = idcs
-    obs = f.readlines()
-    f.close()
+    with open(filename, 'r') as f:
+        idcs = _getColIndices(f, analytes)
 
-    # filter out states for react state (assuming all states are i_soln or react)
+        # exit if failed to read columns
+        if idcs == ValueError:
+            print("failed to parse columns of {}".format(filename))
+            return
+        # assign indices and finish reading the file if getColIndicies didn't error
+        i_sm, i_st, i_elems = idcs
+        obs = f.readlines()
+
+    # filter out states for 'react' state (assuming all states are i_soln or react)
     obs = [re.split(r"[\s,\"]+",ln) for ln in obs if re.split(r"[\s,\"]+",ln)[i_st] == "react"]
-    #print(obs)
-    
-    # these are the simulations:
-#     print([int(row[i_sm]) for row in obs])
-    # these are the concentrations:
-#     print([float(row[i_U]) for row in obs])
 
     # set up output matrix
     # apparently pandas > numpy in cases of > 500K rows
     # http://gouthamanbalaraman.com/blog/numpy-vs-pandas-comparison.html
-    opt = np.array([(int(row[i_sm]), float(row[i_U])) for row in obs]).T
+    
+    # column of sim numbers
+    opt = np.array([int(row[i_sm]) for row in obs])
+    
+    for i_elem in i_elems:
+        values = np.array([float(row[i_elem]) for row in obs])
+        opt = np.vstack((opt, values))
+#         print(opt)
+    
     #opt.sort(0)
     return opt
 
